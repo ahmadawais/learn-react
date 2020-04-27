95cad651bb63f9b1564ab29a704a62689ac689cb
Ahmad Awais <me@AhmadAwais.com>
2020-04-27 12:45:10 +0500

    📖 DOC: Lesson 1 Code

---
+++ course/01/main.js
@@ -0,0 +1,8 @@
+// 1. Access the `root` element.
+const root = document.querySelector('#root');
+
+// 2. Append a heading to the `root` element.
+const heading = document.createElement('h1');
+heading.textContent = 'Crazy! Learn React with Awais';
+
+root.appendChild(heading);
